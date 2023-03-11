0beeb303f8aac1771bd7000da4dc42f7f965ae60
pocari1210 <pocari.design@gmail.com>
2023-03-11 22:06:46 +0900

    events_store編集

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreEventRequest;
 use App\Http\Requests\UpdateEventRequest;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use App\Services\EventService;
 
 class EventController extends Controller
 {
@@ -43,7 +45,46 @@ class EventController extends Controller
      */
     public function store(StoreEventRequest $request)
     {
-        dd($request);
+        // dd($request);
+
+        // $check=DB::table('events')
+        // ->whereDate('start_date',$request['event_date'])
+        // ->whereTime('end_date', '>', $request['start_time'])
+        // ->whereTime('start_date', '<', $request['end_time'])
+        // ->exists();
+
+        // dd($check);
+
+        $check = EventService::checkEventDuplication(
+            $request['event_date'],$request['start_time'],$request['end_time']);        
+
+        if($check){
+            session()->flash('status', 'この時間帯は既に他の予約が存在します。');
+            return view('manager.events.create');
+        }
+
+        // 日付と開始時刻を連結させ、DBに保存
+        // $start = $request['event_date'] . " " . $request['start_time']; 
+        // $startDate = Carbon::createFromFormat( 'Y-m-d H:i', $start );
+        
+        // $end = $request['event_date'] . " " . $request['end_time']; 
+        // $endDate = Carbon::createFromFormat( 'Y-m-d H:i', $end );
+
+        $startDate = EventService::joinDateAndTime($request['event_date'],$request['start_time']);
+        $endDate = EventService::joinDateAndTime($request['event_date'],$request['end_time']);           
+
+        Event::create([
+            'name' => $request['event_name'],
+            'information' => $request['information'],
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'max_people' => $request['max_people'],
+            'is_visible' => $request['is_visible']
+        ]);
+
+        session()->flash('status', '登録okです');
+
+        return to_route('events.index');
     }
 
     /**
