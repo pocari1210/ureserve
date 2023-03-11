6da5634f9499e0d8759e92c6454795b4c99ba7bc
pocari1210 <pocari.design@gmail.com>
2023-03-11 16:06:25 +0900

    createファイル作成

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/flatpickr.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require('postcss-import'),
+        require('tailwindcss'),
     ]);
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
